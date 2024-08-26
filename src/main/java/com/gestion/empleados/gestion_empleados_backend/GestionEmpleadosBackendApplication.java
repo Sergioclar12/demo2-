218499a0fdd0ec7f7e67f3c6c01f087eda23d8f9
Orlando <romerclar123@gmail.com>
2024-08-26 22:42:35 +0200

    Initial commit of backend

--- src/main/java/com/gestion/empleados/gestion_empleados_backend/GestionEmpleadosBackendApplication.java
+++ src/main/java/com/gestion/empleados/gestion_empleados_backend/GestionEmpleadosBackendApplication.java
@@ -3,7 +3,9 @@ package com.gestion.empleados.gestion_empleados_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class GestionEmpleadosBackendApplication {
 
 	public static void main(String[] args) {
